a4728eba3c8acd601ffd85bd7f2479aaa5723352
GeovanaBernardi <104101129+GeovanaBernardi@users.noreply.github.com>
2024-06-06 09:44:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes= document.querySelectorAll(".botao")
-
+const conteudo= document.querySelectorAll("aba.conteudo")
       for( let i = 0 ; i < botoes.length ; i++){
            botoes[i].onclick = function (){
              for (let j = 0; j <botoes.length ; j++){
@@ -8,3 +8,4 @@ const botoes= document.querySelectorAll(".botao")
              botoes[i].classList.add("ativo")
            }
       }
+      
